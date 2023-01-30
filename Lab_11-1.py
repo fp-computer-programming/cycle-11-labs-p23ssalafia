cfa996293a7b2be8920f548146eb5af581880168
Sean Salafia <p23ssalafia@fairfieldprep.org>
2023-01-30 09:17:29 -0500

    11-1 DONE

---
+++ Lab_11-1.py
@@ -0,0 +1,10 @@
+#Author: Sean Salafia 1/30/23
+
+computer_assignments = {2:"Tanner",3:"Jack",5:"Preston",7:"Vishnu",8:"Brian",10:"Richie",13:"Ben",15:"Aiden",17:"Nick",18:"Nathaniel",21:"Colin",24:"Joe",27:"Jan",28:"Jack",30:"Will",34:"Sean",35:"Robbie",36:"Ethan"}
+
+print(computer_assignments)
+
+del computer_assignments[34]
+computer_assignments [1] = "Billy"
+
+print (computer_assignments)
