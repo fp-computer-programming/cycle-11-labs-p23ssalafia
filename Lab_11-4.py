30a297d5158ce83b3a483e57798d91521e15e4a7
Sean Salafia <p23ssalafia@fairfieldprep.org>
2023-02-02 12:30:30 -0500

    Lab 11-4 and 11-5 DONE

---
+++ Lab_11-4.py
@@ -0,0 +1,7 @@
+#Author: Sean Salafia 2/2/23
+
+weekdays = ('Monday','Tuesday','Wednesday','Thursday','Friday')
+weekends = ('Saturday','Sunday')
+days_of_the_week = weekdays + weekends
+
+print(days_of_the_week)
---
+++ Lab_11-5.py
@@ -0,0 +1,9 @@
+#Author: Sean Salafia 2/2/23
+
+day_date = ['2','3','4','5','6']
+(wed,thu,fri,sat,sun) = day_date
+print(wed)
+print(thu)
+print(fri)
+print(sat)
+print(sun)
