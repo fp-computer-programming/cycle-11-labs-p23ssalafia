9ef20dc48e117811e3948bb743f8b914896048e2
Sean Salafia <p23ssalafia@fairfieldprep.org>
2023-02-03 13:47:56 -0500

    CODEWARS WIP

---
+++ Lab_11-6_Codewars.py
@@ -0,0 +1,167 @@
+#Author: Sean Salafia
+
+def ordered_count(input):
+    tuples = ()
+characters = [*input]
+for letters in characters:
+    if letters == 'a':
+        sum_a =+ 1
+        key_a = 'a'
+    if letters == 'b':
+        sum_b =+ 1
+        key_b = 'a'
+    if letters == 'c':
+        sum_c =+ 1
+        key_c = 'a'
+    if letters == 'd':
+        sum_d =+ 1
+        key_d = 'a'
+    if letters == 'e':
+        sum_e =+ 1
+        key_e = 'a'
+    if letters == 'f':
+        sum_f =+ 1
+        key_f = 'a'
+    if letters == 'g':
+        sum_g =+ 1
+        key_g = 'a'
+    if letters == 'h':
+        sum_h =+ 1
+        key_h = 'a'
+    if letters == 'i':
+        sum_i =+ 1
+        key_i = 'a'
+    if letters == 'j':
+        sum_j =+ 1
+        key_j = 'a'
+    if letters == 'k':
+        sum_k =+ 1
+        key_k = 'a'
+    if letters == 'l':
+        sum_l =+ 1
+        key_l = 'a'
+    if letters == 'm':
+        sum_m =+ 1
+        key_m = 'a'
+    if letters == 'n':
+        sum_n =+ 1
+        key_n = 'a'
+    if letters == 'o':
+        sum_o =+ 1
+        key_o = 'a'
+    if letters == 'p':
+        sum_p =+ 1
+        key_p = 'a'
+    if letters == 'q':
+        sum_q =+ 1
+        key_q = 'a'
+    if letters == 'r':
+        sum_r =+ 1
+        key_r = 'a'
+    if letters == 's':
+        sum_s =+ 1
+        key_s = 'a'
+    if letters == 't':
+        sum_t =+ 1
+        key_t = 'a'
+    if letters == 'u':
+        sum_u =+ 1
+        key_u = 'a'
+    if letters == 'v':
+        sum_v =+ 1
+        key_v = 'a'
+    if letters == 'w':
+        sum_w =+ 1
+        key_w = 'a'
+    if letters == 'x':
+        sum_x =+ 1
+        key_x = 'a'
+    if letters == 'y':
+        sum_y =+ 1
+        key_y = 'a'
+    if letters == 'z':
+        sum_z =+ 1
+        key_z = 'a'
+    if letters == ' ':
+        sum_space =+ 1
+        key_space = 'a'
+    if letters == 'A':
+        sum_a =+ 1
+        key_A = 'a'
+    if letters == 'B':
+        sum_b =+ 1
+        key_B = 'a'
+    if letters == 'C':
+        sum_c =+ 1
+        key_B = 'a'
+    if letters == 'D':
+        sum_d =+ 1
+        key_D = 'a'
+    if letters == 'E':
+        sum_e =+ 1
+        key_E = 'a'
+    if letters == 'F':
+        sum_f =+ 1
+        key_a = 'a'
+    if letters == 'G':
+        sum_g =+ 1
+        key_a = 'a'
+    if letters == 'H':
+        sum_h =+ 1
+        key_a = 'a'
+    if letters == 'I':
+        sum_i =+ 1
+        key_a = 'a'
+    if letters == 'J':
+        sum_j =+ 1
+        key_a = 'a'
+    if letters == 'K':
+        sum_k =+ 1
+        key_a = 'a'
+    if letters == 'L':
+        sum_l =+ 1
+        key_a = 'a'
+    if letters == 'M':
+        sum_m =+ 1
+        key_a = 'a'
+    if letters == 'N':
+        sum_n =+ 1
+        key_a = 'a'
+    if letters == 'O':
+        sum_o =+ 1
+        key_a = 'a'
+    if letters == 'P':
+        sum_p =+ 1
+        key_a = 'a'
+    if letters == 'Q':
+        sum_q =+ 1
+        key_a = 'a'
+    if letters == 'R':
+        sum_r =+ 1
+        key_a = 'a'
+    if letters == 'S':
+        sum_s =+ 1
+        key_a = 'a'
+    if letters == 'T':
+        sum_t =+ 1
+        key_a = 'a'
+    if letters == 'U':
+        sum_u =+ 1
+        key_a = 'a'
+    if letters == 'V':
+        sum_v =+ 1
+        key_a = 'a'
+    if letters == 'W':
+        sum_w =+ 1
+        key_a = 'a'
+    if letters == 'X':
+        sum_x =+ 1
+        key_a = 'a'
+    if letters == 'Y':
+        sum_y =+ 1
+        key_a = 'a'
+    if letters == 'Z':
+        sum_z =+ 1
+        key_a = 'a'
+
+        
