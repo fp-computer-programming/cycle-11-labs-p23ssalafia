6e66f87badd930b989d16845e775b32290cebcf9
Sean Salafia <p23ssalafia@fairfieldprep.org>
2023-02-10 10:15:52 -0500

    11-4 DONE

--- Lab_11-4.py
+++ Lab_11-4.py
@@ -1,5 +1,7 @@
 #Author: Sean Salafia 2/2/23
 
+
+#Comments not necessary
 weekdays = ('Monday','Tuesday','Wednesday','Thursday','Friday')
 weekends = ('Saturday','Sunday')
 days_of_the_week = weekdays + weekends
